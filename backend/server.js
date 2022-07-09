19bd89a34c2020baabefc11889d70348a251a231
Talal <talalfarrukh200@gmail.com>
2022-07-09 13:28:32 +0500

    heruko error

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser');
 const client = require('./db.js')
 const cors = require('cors')

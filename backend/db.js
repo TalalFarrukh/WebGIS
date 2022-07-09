14dd79cdbded379bf8ceb58dcf48c17aaae7d3cf
Talal <talalfarrukh200@gmail.com>
2022-07-09 16:23:56 +0500

    change git ignore

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,7 @@ const devConfig = {
   port: process.env.PG_PORT,
 }
 
-const proConfig = 'postgres://bbdmahbnyjecsj:869c863bf7eb796c8a653a5b47a5ecf6fa4777a833f247816f5fc75ad2895da6@ec2-3-223-169-166.compute-1.amazonaws.com:5432/ds7kr27i20hnu'
+const proConfig = process.env.DATABASE_URL
 
 const client = new Client(
     process.env.NODE_ENV === "production" ? proConfig : devConfig
